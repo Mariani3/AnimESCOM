089614903e5bc79e6bc95c0f21fe3b8a4dd6e84d
Mariani3 <45346676+Mariani3@users.noreply.github.com>
2019-10-17 10:47:56 -0500

    Update Elenco.java

--- src/java/Clases/Elenco.java
+++ src/java/Clases/Elenco.java
@@ -9,6 +9,68 @@ package Clases;
  *
  * @author aresu
  */
-public class Elenco {
+public class Elenco {private String nombre;
+    private String apeido;
+    private String fechaDeNacimiento;
+    private String nacionalidad;
+    
+    public Elenco(){
+        nombre="nombre";
+        apeido="apeido";
+        fechaDeNacimiento="algunafecha";
+        nacionalidad="algunpais";
+    }
+    
+    public Elenco(String nombre, String apeido, String fechaDeNacimiento, String nacionalidad){
+        this.nombre=nombre;
+        this.apeido=apeido;
+        this.fechaDeNacimiento=fechaDeNacimiento;
+        this.nacionalidad=nacionalidad;
+    }
+    
+    public Elenco(Elenco e){
+        this.nombre=e.nombre;
+        this.apeido=e.apeido;
+        this.fechaDeNacimiento=e.fechaDeNacimiento;
+        this.nacionalidad=e.nacionalidad;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Elenco other = (Elenco) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.apeido, other.apeido)) {
+            return false;
+        }
+        if (!Objects.equals(this.fechaDeNacimiento, other.fechaDeNacimiento)) {
+            return false;
+        }
+        if (!Objects.equals(this.nacionalidad, other.nacionalidad)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Elenco{" + "nombre=" + nombre + ", apeido=" + apeido + ", fechaDeNacimiento=" + fechaDeNacimiento + ", nacionalidad=" + nacionalidad + '}';
+    }
     
 }
